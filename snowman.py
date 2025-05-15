f4e96320a8fb128df57a3193921b65da158e0ead
Anastasia Kiessig <anastasia.sibir@gmail.com>
2025-05-15 10:01:46 +0200

    Create new file game_logic.py and place all relevant methods there.

--- snowman.py
+++ snowman.py
@@ -1,46 +1,4 @@
-import random
-import ascii_art
-
-# List of secret words
-WORDS = ["python", "git", "github", "snowman", "meltdown"]
-
-
-def get_random_word():
-    """Selects a random word from the list."""
-    return WORDS[random.randint(0, len(WORDS) - 1)]
-
-
-def display_game_state(mistakes, secret_word, guessed_letters):
-    """
-    Display the snowman stage for the current number of mistakes.
-    :param mistakes: number of mistakes that the user did by guessing the word
-    :param secret_word: string that should be guessed
-    :param guessed_letters: list with letters that the user has already guessed
-    """
-    print(ascii_art.STAGES[mistakes])
-    # Build a display version of the secret word.
-    display_word = ""
-    for letter in secret_word:
-        if letter in guessed_letters:
-            display_word += letter + " "
-        else:
-            display_word += "_ "
-    print("Word: ", display_word)
-    print("\n")
-
-
-def play_game():
-    secret_word = get_random_word()
-    guessed_letters = []
-    mistakes = 0
-
-    print("Welcome to Snowman Meltdown!")
-    print("Secret word selected: " + secret_word)  # for testing, later remove this line
-
-    display_game_state(mistakes, secret_word, guessed_letters)
-    # For now, simply prompt the user once:
-    guess = input("Guess a letter: ").lower()
-    print("You guessed:", guess)
+from game_logic import play_game
 
 
 if __name__ == "__main__":
