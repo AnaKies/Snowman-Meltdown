d87bf581130b1ff21d4ede9852c5b4043af151ec
Anastasia Kiessig <anastasia.sibir@gmail.com>
2025-05-15 09:54:49 +0200

    Add method display_game_state to display the first stage of a snowman and letter7placeholders for the secret word

--- snowman.py
+++ snowman.py
@@ -1,4 +1,5 @@
 import random
+import grafics
 
 # List of secret words
 WORDS = ["python", "git", "github", "snowman", "meltdown"]
@@ -9,12 +10,34 @@ def get_random_word():
     return WORDS[random.randint(0, len(WORDS) - 1)]
 
 
+def display_game_state(mistakes, secret_word, guessed_letters):
+    """
+    Display the snowman stage for the current number of mistakes.
+    :param mistakes: number of mistakes that the user did by guessing the word
+    :param secret_word: string that should be guessed
+    :param guessed_letters: list with letters that the user has already guessed
+    """
+    print(grafics.STAGES[mistakes])
+    # Build a display version of the secret word.
+    display_word = ""
+    for letter in secret_word:
+        if letter in guessed_letters:
+            display_word += letter + " "
+        else:
+            display_word += "_ "
+    print("Word: ", display_word)
+    print("\n")
+
+
 def play_game():
     secret_word = get_random_word()
+    guessed_letters = []
+    mistakes = 0
+
     print("Welcome to Snowman Meltdown!")
     print("Secret word selected: " + secret_word)  # for testing, later remove this line
 
-    # TODO: Build your game loop here.
+    display_game_state(mistakes, secret_word, guessed_letters)
     # For now, simply prompt the user once:
     guess = input("Guess a letter: ").lower()
     print("You guessed:", guess)
