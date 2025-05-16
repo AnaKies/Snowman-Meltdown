929e42c903e2489215d9b7bd9aed9da491a22bc5
Anastasia Kiessig <anastasia.sibir@gmail.com>
2025-05-16 14:26:33 +0200

    Add new stages to the snowman graphics.

--- ascii_art.py
+++ ascii_art.py
@@ -7,21 +7,49 @@ STAGES = [
      (◉_◉) 
      ( : ) 
      ( : )
+_______________
      """,
-     # Stage 1: Bottom part starts melting
+     # Stage 1 Bottom part melts
+    """
+     ___  
+    /___\\
+    (◉_◉) 
+    ( : ) 
+   (  :  )
+~~~~~~~~~~~~~~
+    """,
+     # Stage 2: Bottom part disappears
      """
       ___  
      /___\\
      (◉_◉) 
      ( : )
+~~~~~~~~~~~~~~
      """,
-     # Stage 2: Only the head remains
+    # Stage 3: Middle part melts
+    """
+     ___  
+    /___\\
+    (◉_◉) 
+   (  :  )
+~~~~~~~~~~~~~~
+    """,
+     # Stage 4: Only the head remains
      """
       ___  
      /___\\
      (◉口◉)
+~~~~~~~~~~~~~~
      """,
-     # Stage 3: Snowman completely melted
+    # Stage 5: Head disappears
+    """
+      ___  
+     /___\\
+     ｡ ◉｡ 
+     ´   `
+~~~~~~~~~~~~~~
+    """,
+     # Stage 6: Snowman completely melted
      """
       ___  
      /___\\
