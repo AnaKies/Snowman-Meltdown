573563c7e42ea926dc796d6e3f3a2ce03b9ba14b
Anastasia Kiessig <anastasia.sibir@gmail.com>
2025-05-16 10:26:42 +0200

    Update method play_game whith a cycle calling logic. Add to the method display_game_state a mistake counter and a interrupt condition.

--- ascii_art.py
+++ ascii_art.py
@@ -3,27 +3,27 @@ STAGES = [
      # Stage 0: Full snowman
      """
       ___  
-     /___\\ 
-     (o o) 
+     /___\\
+     (◉_◉) 
      ( : ) 
-     ( : ) 
+     ( : )  "I am a nice snowman." 
      """,
      # Stage 1: Bottom part starts melting
      """
       ___  
-     /___\\ 
-     (o o) 
-     ( : ) 
+     /___\\
+     (◉_◉) 
+     ( : )  "Oh no, your mistake cost me my legs!"
      """,
      # Stage 2: Only the head remains
      """
       ___  
-     /___\\ 
-     (o o) 
+     /___\\
+     (◉口◉)  "Save me! Please guess the right letter!!!"
      """,
      # Stage 3: Snowman completely melted
      """
       ___  
-     /___\\ 
+     /___\\ "The snowman has gone on vacation to a warmer place!"
      """
  ]
--- game_logic.py
+++ game_logic.py
@@ -17,17 +17,37 @@ def display_game_state(mistakes, secret_word, guessed_letters):
     :param mistakes: number of mistakes that the user did by guessing the word
     :param secret_word: string that should be guessed
     :param guessed_letters: list with letters that the user has already guessed
+    :return: None
     """
     print(ascii_art.STAGES[mistakes])
+
+    if mistakes >= len(ascii_art.STAGES) - 1:
+        print(f"Game Over! The secret word was: {secret_word}")
+        return None
+
     # Build a display version of the secret word.
     display_word = ""
     for letter in secret_word:
         if letter in guessed_letters:
             display_word += letter + " "
         else:
+            is_wrong = True
             display_word += "_ "
     print("Word: ", display_word)
     print("\n")
+    return is_wrong
+
+def check_wrong_input(user_letter):
+    """
+    Checks if user enters a single letter, and it is not a digit.
+    :param user_letter: guessed letter from user input
+    """
+    if len(user_letter) != 1:
+        raise Exception(f"Only single characters are allowed! You entered {user_letter}.")
+        return
+
+    if user_letter.isdigit():
+        raise Exception(f"Digits are not allowed! You entered {user_letter}.")
 
 
 def play_game():
@@ -38,7 +58,15 @@ def play_game():
     print("Welcome to Snowman Meltdown!")
     print("Secret word selected: " + secret_word)  # for testing, later remove this line
 
-    display_game_state(mistakes, secret_word, guessed_letters)
-    # For now, simply prompt the user once:
-    guess = input("Guess a letter: ").lower()
-    print("You guessed:", guess)
+    while True:
+        is_wrong = display_game_state(mistakes, secret_word, guessed_letters)
+
+        if is_wrong is True:
+            mistakes += 1
+        elif is_wrong is None:
+            break
+
+        user_letter = input("Guess a letter: ").lower()
+        guessed_letters.append(user_letter)
+        print("You guessed:", user_letter)
+        check_wrong_input(user_letter)
