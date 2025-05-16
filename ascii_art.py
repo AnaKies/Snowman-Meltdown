902d1d0bff9ace3979a61908fd778b364cb70580
Anastasia Kiessig <anastasia.sibir@gmail.com>
2025-05-16 15:04:26 +0200

    Code formatting.

--- ascii_art.py
+++ ascii_art.py
@@ -1,15 +1,15 @@
 # Snowman ASCII Art stages
 STAGES = [
-     # Stage 0: Full snowman
-     """
-      ___  
-     /___\\
-     (◉_◉) 
-     ( : ) 
-     ( : )
+    # Stage 0: Full snowman
+    """
+     ___  
+    /___\\
+    (◉_◉) 
+    ( : ) 
+    ( : )
 _______________
-     """,
-     # Stage 1 Bottom part melts
+    """,
+    # Stage 1 Bottom part melts
     """
      ___  
     /___\\
@@ -18,14 +18,14 @@ _______________
    (  :  )
 ~~~~~~~~~~~~~~
     """,
-     # Stage 2: Bottom part disappears
-     """
-      ___  
-     /___\\
-     (◉_◉) 
-     ( : )
+    # Stage 2: Bottom part disappears
+    """
+     ___  
+    /___\\
+    (◉_◉) 
+    ( : )
 ~~~~~~~~~~~~~~
-     """,
+    """,
     # Stage 3: Middle part melts
     """
      ___  
@@ -34,13 +34,13 @@ _______________
    (  :  )
 ~~~~~~~~~~~~~~
     """,
-     # Stage 4: Only the head remains
-     """
-      ___  
-     /___\\
-     (◉口◉)
+    # Stage 4: Only the head remains
+    """
+     ___  
+    /___\\
+    (◉口◉)
 ~~~~~~~~~~~~~~
-     """,
+    """,
     # Stage 5: Head disappears
     """
       ___  
@@ -49,9 +49,9 @@ _______________
      ´   `
 ~~~~~~~~~~~~~~
     """,
-     # Stage 6: Snowman completely melted
-     """
-      ___  
-     /___\\
-     """
- ]
+    # Stage 6: Snowman completely melted
+    """
+     ___  
+    /___\\
+    """
+]
--- game_logic.py
+++ game_logic.py
@@ -1,16 +1,19 @@
 import random
-import ascii_art
+
 from colorama import init, Fore
 
+# Module with graphic for different stages of snowman melting.
+import ascii_art
 
 # List of secret words
 WORDS = ["python", "git", "github", "snowman", "meltdown"]
 SEPARATORS_NUMBER = 40
 
+
 def get_random_word():
     """
     Selects a random word from the list.
-    :return: a random word from the list.
+    :return: A random word from the list.
     """
     return WORDS[random.randint(0, len(WORDS) - 1)]
 
@@ -19,19 +22,19 @@ def display_game_state(mistakes, secret_word, guessed_letters):
     """
     Displays the snowman stage for the current number of mistakes.
     Registers a mistake, evaluates and displays a game over.
-    :param mistakes: number of mistakes that the user did by guessing the word
+    :param mistakes: Number of mistakes that the user did by guessing the word
     :param secret_word: string that should be guessed
     :param guessed_letters: list with letters that the user has already guessed
-    :return: True if the game is won, False if game is over, None otherwise
+    :return: True if the game is won, False if the game is over, None otherwise
     """
     print(Fore.LIGHTBLUE_EX + "*" * SEPARATORS_NUMBER)
     print(Fore.BLUE + ascii_art.STAGES[mistakes])
-    print(Fore.LIGHTBLUE_EX +  "." * SEPARATORS_NUMBER)
+    print(Fore.LIGHTBLUE_EX + "." * SEPARATORS_NUMBER)
 
-    # last stage of a snowman (hat) means game over
+    # the last stage of a snowman (hat) means game over
     if mistakes >= len(ascii_art.STAGES) - 1:
         print(Fore.RED + f"💀Game Over! The secret word was: {secret_word}")
-        return False # game over
+        return False  # game over
 
     # Build a display version of the secret word.
     display_word = ""
@@ -40,28 +43,30 @@ def display_game_state(mistakes, secret_word, guessed_letters):
             display_word += letter + " "
         else:
             display_word += "_ "
-    print(Fore.GREEN +  "Word: ", display_word)
-    print(Fore.LIGHTBLUE_EX +  "." * SEPARATORS_NUMBER)
+    print(Fore.GREEN + "Word: ", display_word)
+    print(Fore.LIGHTBLUE_EX + "." * SEPARATORS_NUMBER)
 
-    if not "_" in display_word:
+    if "_" not in display_word:
         print("🎉Congratulations, you saved the snowman!")
-        return True # game is won
-    return None # game go further
+        return True  # the game is won
+    return None  # game go further
 
 
 def check_wrong_input(user_letter):
     """
-    Checks if user enters a single letter, and it is not a digit.
-    :param user_letter: guessed letter from user input
+    Checks if a user enters a single letter, and it is not a digit.
+    :param user_letter: guessed a letter from user input
     :return: True if the user did a wrong input, False otherwise.
     """
     if len(user_letter) != 1:
-        print(Fore.RED +  f"Only single characters are allowed! You entered {user_letter}.")
+        print(Fore.RED +
+              f"Only single characters are allowed! "
+              f"You entered {user_letter}.")
         wrong_input = True
         return wrong_input
 
     if user_letter.isdigit():
-        print(Fore.RED +  f"Digits are not allowed! You entered {user_letter}.")
+        print(Fore.RED + f"Digits are not allowed! You entered {user_letter}.")
         wrong_input = True
         return wrong_input
 
@@ -72,16 +77,19 @@ def check_wrong_input(user_letter):
 def init_new_game():
     """
     Initializes a new game of snowman.
-    :return: secret word as string, guessed letters as list, mistakes as integer
+    :return: Secret word as string,
+    guessed letters as a list,
+    mistakes as integer
     """
     secret_word = get_random_word()
     print(Fore.MAGENTA + "❄️Welcome to Snowman Meltdown!")
-    print(Fore.LIGHTYELLOW_EX + "Secret word selected: " + secret_word)  # for testing, later remove this line
+    print(Fore.LIGHTYELLOW_EX +
+          "Secret word selected: " +
+          secret_word)  # for testing, later remove this line
     guessed_letters = []
     mistakes = 0
 
     return mistakes, secret_word, guessed_letters
-
 
 
 def play_game():
@@ -89,24 +97,25 @@ def play_game():
     Runs the game logic and displays the game state.
     :return: None
     """
-    init(autoreset = True) # initialise colorama module with reset of current color
+    # initialise colorama module with reset of current color
+    init(autoreset=True)
     mistakes, secret_word, guessed_letters = init_new_game()
     while True:
         try:
-            game_is_won = display_game_state(mistakes, secret_word, guessed_letters)
-
+            game_is_won = display_game_state(mistakes,
+                                             secret_word,
+                                             guessed_letters)
             # stop the game if it was won or lost
             if game_is_won is not None:
                 restart_game = input("Restart game? Y/N? ")
                 if restart_game.lower() == "n":
                     break
-                elif restart_game.lower() == "y":
+                if restart_game.lower() == "y":
                     print()
                     mistakes, secret_word, guessed_letters = init_new_game()
                     continue
-                else:
-                    print("Print Y or N")
-                    continue
+                print("Print Y or N")
+                continue
 
             user_letter = input("Guess a letter: ").lower()
             wrong_user_input = check_wrong_input(user_letter)
@@ -116,7 +125,7 @@ def play_game():
 
             letter_is_in_secret_word = user_letter in secret_word
 
-            if not letter_is_in_secret_word is True:
+            if letter_is_in_secret_word is not True:
                 mistakes += 1
         except Exception as error:
-            print(Fore.RED +  f"Error in program: {error}")
+            print(Fore.RED + f"Error in program: {error}")
