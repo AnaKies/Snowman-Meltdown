8f1ee9328b672093c9dc20271638f44f1e79599e
Anastasia Kiessig <anastasia.sibir@gmail.com>
2025-05-16 11:39:13 +0200

    Delete phrases of a snowman.

--- ascii_art.py
+++ ascii_art.py
@@ -6,24 +6,24 @@ STAGES = [
      /___\\
      (◉_◉) 
      ( : ) 
-     ( : )  "I am a nice snowman." 
+     ( : )
      """,
      # Stage 1: Bottom part starts melting
      """
       ___  
      /___\\
      (◉_◉) 
-     ( : )  "Oh no, your mistake cost me my legs!"
+     ( : )
      """,
      # Stage 2: Only the head remains
      """
       ___  
      /___\\
-     (◉口◉)  "Save me! Please guess the right letter!!!"
+     (◉口◉)
      """,
      # Stage 3: Snowman completely melted
      """
       ___  
-     /___\\ "The snowman has gone on vacation to a warmer place!"
+     /___\\
      """
  ]
